746aedd7d15b4010d3c14ac036b9d343c1bc2441
nowwwwi <kouimai.0720@gmail.com>
2024-05-06 15:47:59 +0900

    login feature

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'houseworks.apps.HouseworksConfig',
+    'accounts.apps.AccountsConfig',
     'django_bootstrap5',
 ]
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("accounts.urls")),
     path("houseworks/", include("houseworks.urls")),
     path('admin/', admin.site.urls),
 ]
