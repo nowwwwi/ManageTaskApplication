315abfca00fac22edf37b1214ecab53597cf62aa
今井 航平 <k-imai@kip.co.jp>
2024-06-02 17:03:47 +0900

    ADD:postgres

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taskdatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5434',
     }
 }
 
